1a32fc462a383ec5033b0f38a1bda2bdb6f995e4
Brian Lee <brianjleeofcl@gmail.com>
2017-09-03 23:00:39 -0700

    HTTP: deploy

--- http/src/main/java/App.java
+++ http/src/main/java/App.java
@@ -3,11 +3,24 @@ import com.sun.net.httpserver.HttpServer;
 
 import java.net.InetSocketAddress;
 
+import static java.lang.Integer.parseInt;
+
 public class App {
+    private static int port;
+
+    static {
+        try {
+            port = parseInt(System.getenv("PORT"));
+        } catch (NumberFormatException e) {
+            port = 5000;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         server.createContext("/example", new ExampleHandler());
         server.setExecutor(null);
         server.start();
+        System.out.printf("Server listening at port %d.%n", port);
     }
 }
